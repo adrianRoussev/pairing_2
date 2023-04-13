c9c203a2b0a690f04876294546d3fde18e21c074
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-04-12 20:09:00 -0400

    add more conditionals

--- plants.rb
+++ plants.rb
@@ -1,7 +1,11 @@
-gets_sunshine = true
+gets_sunshine = false
 ounces_of_water = 10
-if gets_sunshine = true && ounces_of_water >= 10
+if gets_sunshine == true && ounces_of_water >= 10
     puts "the plant will thrive"
-else  
-    puts "the plant will die"
+elsif gets_sunshine == true && ounces_of_water < 10   
+    puts "the plant will die, give it more water"
+elsif gets_sunshine == false && ounces_of_water >= 10
+    puts "the plant will die, give it more sunshine"
+else 
+puts "you are a bad plant parent" 
 end
